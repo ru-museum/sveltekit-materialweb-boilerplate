ea1deecdc1062db98131709215687e769768d1c1
ru-museum <museum_ru@yahoo.co.jp>
2023-07-13 04:29:56 +0900

    Create menues.js

---
+++ src/lib/data/menues.js
@@ -0,0 +1,31 @@
+ 
+const menues = [
+		{
+			category: 'CATEGORY I',
+			items: [
+					      {name:"HOME", ref:"/"},
+      					{name:"WORK", ref:"work"},
+					      {name:"ABOUT", ref:"about"}
+       			 ]
+		},
+		{
+			category: 'CATEGORY II',
+			items: [
+					      {name:"HOME", ref:"/"},
+      					{name:"WORK", ref:"work"},
+					      {name:"ABOUT", ref:"about"}
+       			 ]
+		},
+		{
+			category: 'CATEGORY III',
+			items: [
+					      {name:"HOME", ref:"/"},
+      					{name:"WORK", ref:"work"},
+					      {name:"ABOUT", ref:"about"}
+       			 ]
+		}
+	
+	]
+
+export default menues;
+

a7518a58c4673cd3174f5bd2b0a02a1e13e1edd2
ru-museum <museum_ru@yahoo.co.jp>
2023-07-12 09:08:15 +0900

    Add files via upload

---
+++ src/routes/work/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+	<title>Work</title>
+	<meta name="description" content="WORK this app" />
+</svelte:head>
+
+<div class="text-column">
+	<h1 class="mdc-typography--headline1">WORK</h1>
+	<h2>
+		<strong>src/routes/<span style="color:#800000;">work</span>/+page.svelte</strong>
+	</h2>
+	
+  {#each new Array(40) as {}}
+    <br>
+  {/each}
+	
+</div>

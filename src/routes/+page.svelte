be4317badef13db50cd4418d08ee5d8529598dcb
ru-museum <museum_ru@yahoo.co.jp>
2026-04-17 06:34:02 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,6 +17,34 @@
 	<h2>
 		<strong>src/routes/+page.svelte</strong>
 	</h2>
+<h1>USAGE</h1>
+<ol>
+  <li><h3>package.json</h3>
+<pre>
+"@fontsource/fira-mono": "^5.2.7",
+"@neoconfetti/svelte": "^2.2.2",
+"@sveltejs/adapter-auto": "^7.0.1",
+"@sveltejs/adapter-static": "^3.0.10",
+"@sveltejs/kit": "^2.57.1",
+"@sveltejs/vite-plugin-svelte": "^7.0.0",
+"@types/cookie": "^1.0.0",
+"svelte": "^5.55.4",
+"svelte-check": "^4.4.6",
+"svelte-preprocess": "^6.0.3",
+"typescript": "^5.9.3",
+"vite": "^8.0.8"
+</pre>
+※ typescript は svelte-preprocess 依存のため "<strong>5.9.3</strong>（latest: 6.0.2）" です。
+  </li>
+  <li><h3>npm install: warn deprecated</h3>
+<pre>
+// 以下の警告が表示されます(MWC)
+npm warn deprecated @material/mwc-top-app-bar@0.27.0: 
+  MWC beta is longer supported. Please upgrade to @material/web
+....（略）....
+</pre>
+</li>
+</ol>
 
 <h1>LINKS</h1>
 <a href="https://github.com/material-components/material-web/tree/v0.27.0/packages" target="_blank" rel="noreferrer">material-web（MWC v0.27.0）</a><br>
